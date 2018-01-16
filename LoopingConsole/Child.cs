41efcb1e8b0b311a68ee50883b5d2fbfa8c4367b
Kelly Stoops <kstoops815@gmail.com>
2018-01-15 20:50:25 -0600

    created some children

---
+++ LoopingConsole/Child.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopingConsole
+{
+    class Child
+    {
+        public string Name { get; set; }
+        public bool Sick { get; set; }
+
+        //everything up to this point is called a POCO - plain old C# object, when methods are added it is no longer a POCO
+
+        public string ForgeASickNote()
+        {
+            /* this is a conditional
+            if (Sick)
+            {
+                return $"Please excuse {Name} from class today becaue they are very sick.";
+            }
+
+            return $"{Name}, you still have to go to school, quit faking. Love Mom";
+            */
+
+            // this is the same as an if statement but uses a terenary
+            return Sick
+                ? $"Please excuse {Name} from class today because they are very sick."
+                : $"{Name}, you still have to go to school, quit faking. Love Mom";
+           
+        }
+    }
+}
--- LoopingConsole/Program.cs
+++ LoopingConsole/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LoopingConsole
 {
@@ -10,6 +7,45 @@ namespace LoopingConsole
     {
         static void Main(string[] args)
         {
+            /* A lot of repetitive code here
+            var suzie = new Child { Name = "Suzie", Sick = false};
+            //child.Name = "Suzie"; both of these are used without object initialization as illustrated above
+            //child.Sick = true;
+
+            Console.WriteLine($"{suzie.Name} is {(suzie.Sick ? "sick" : "healthy")}");
+            Console.WriteLine(suzie.ForgeASickNote());
+
+            var ralph = new Child { Name = "Ralph", Sick = true };
+            //child.Name = "Suzie"; both of these are used without object initialization as illustrated above
+            //child.Sick = true;
+
+            Console.WriteLine($"{ralph.Name} is {(ralph.Sick ? "sick" : "healthy")}");
+            Console.WriteLine(ralph.ForgeASickNote());
+
+            Console.WriteLine("This is our second time talking about the same thing. #No judgement.");
+
+            Console.ReadLine();
+            */
+
+            //This is a better way to do it
+
+            var children = new List<Child>
+            {
+                new Child { Name = "Suzie", Sick = false },
+                new Child { Name = "Ralph", Sick = true }
+            };
+
+            foreach (var child in children)
+            {
+                Console.WriteLine($"{child.Name} is {(child.Sick ? "sick" : "healthy")}");
+                Console.WriteLine(child.ForgeASickNote());
+            }
+            
+   
+
+            Console.WriteLine("This is our second time talking about the same thing. #No judgement.");
+
+            Console.ReadLine();
         }
     }
 }
